a8e00d88b9fc3032f2c73991d24ccec6c880e4c7
Ph@ntom <586pboy@gmail.com>
2020-12-17 01:08:05 +0530

    Updates - 1 - BaseApp/urls.py 1_1 - added routes for 'forgot_password/' and 'reset_password/'. 2 - BaseApp/views.py 2_1 - added logged_in param. in Cart and Checkout and Confirmation CBVs. 2_2 - added ForgotPassword() and ResetPassword() CBV's. 3 - added Base/ forgotPass.html and resetPass.html.

--- Hasan_ECommerce/BaseApp/urls.py
+++ Hasan_ECommerce/BaseApp/urls.py
@@ -1,15 +1,10 @@
 from django.urls import path, include
 from django.contrib.auth.decorators import login_required
 from .views import (
-    IndexView,
-    ProductsView,
-    ProductsByCategory,
-    ProductDesc,
-    AddToCart,
-    Cart,
-    Update_Quantity,
-    Checkout,
-    Confirmation
+    IndexView, ProductsView, ProductsByCategory,
+    ProductDesc, AddToCart, Cart, Update_Quantity,
+    Checkout, Confirmation, ForgotPassword,
+    ResetPassword
 )
 
 app_name = 'base_app'
@@ -18,6 +13,8 @@ urlpatterns = [
     path('cart/', login_required(Cart.as_view()), name="cart"),
     path('checkout/', login_required(Checkout.as_view()), name="checkout"),
     path('confirmation/', login_required(Confirmation.as_view()), name="confirm"),
+    path('forgot_password/', login_required(ForgotPassword.as_view()), name="forgot-pass"),
+    path('reset_password/', login_required(ResetPassword.as_view()), name="reset-pass"),
     path('u_qty/<str:opr>/<int:cloth_id>/', Update_Quantity, name="update_quantity"),
     path('add_to_cart/<str:pg>/<int:id>/<str:mrp>/<str:gender>/<str:category>/', AddToCart, name="add_to_cart"),
     path('products/<str:type>/', ProductsView.as_view(), name="product"),
